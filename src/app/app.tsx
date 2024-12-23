b0a29b444113192f3c05d73442af0fc93820f3a3
Jsqadro <hristolubovasi@gmail.com>
2024-12-23 23:08:15 +0500

    1
    1

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,6 @@
 import { AppRoute } from '@const';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { getAuthorizationStatus } from 'store/user-process/selectors';
 import PrivateRoute from '../components/private-route/private-route';
 import { useAppSelector } from '../hooks/index';
 import FavoritesPage from '../pages/favorites-page/favorites-page';
@@ -9,6 +8,7 @@ import { LoginPage } from '../pages/login-page/login-page';
 import MainPage from '../pages/main-page/main-page';
 import NotFoundPage from '../pages/not-found-page/not-found-page';
 import { OfferPage } from '../pages/offer-page/offer-page';
+import { getAuthorizationStatus } from '../store/user-process/selectors';
 
 export default function App(): JSX.Element {
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
